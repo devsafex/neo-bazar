e5cc394a8f4643bab84104efa2de923011fc8eba
Sanjida <sanjida.ice.778@gmail.com>
2025-02-05 00:49:40 +0600

    components check

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Button from "../../Shaired/Button";
 
 
-
-
 const Home = () => {
+      // const [isLoading, setisLoading] = useState(false)
+      
       return (
             <div className=" flex gap-6">
+
                   <Button>This is shaired btn</Button>
+            
                   <Button>This is shaired btn2</Button>
                    {/* Hello i am home */}
             </div>
