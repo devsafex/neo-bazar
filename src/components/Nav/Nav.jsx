a4b1c636c9840941836ce196d2c28af25599471b
Mehefuj Ali <mehefujalim@gmail.com>
2025-02-05 01:59:24 +0530

    nav bar bg color changed and conditional color logo creat ed

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,20 +4,20 @@ import { IconButton } from "@material-tailwind/react";
 
 const Nav = () => {
   return (
-    <div>
+    <div className=" bg-[#00C982]">
       <div className=" container mx-auto flex items-center justify-between py-2">
         <div>
-          <Logo />
+          <Logo whiteLogo />
         </div>
         <div>
-          <div className=" relative">
+          <div className=" relative text-white">
             <IoMdSearch  className=" text-2xl absolute top-1 md:top-2 right-2" />
-          <input type="text" placeholder="Search..." className=" border py-1 pr-8 w-full md:w-96 md:py-2  px-3 md:px-5 rounded" />
+          <input type="text" placeholder="Search..." className=" focus:outline-0 border py-1 pr-8 w-full md:w-96 md:py-2  px-3 md:px-5 rounded" />
           </div>
         </div>
         <div>
-          <IconButton variant="text" className="  rounded-full md:border-2  border-[#00C982]  select-none h-10 w-10  cursor-pointer">
-            <IoMdCart className="  text-2xl text-[#00C982]" />
+          <IconButton variant="text" className="  rounded-full md:border-2  border-white  select-none h-10 w-10  cursor-pointer">
+            <IoMdCart className="  text-2xl text-white" />
           </IconButton>
         </div>
       </div>
