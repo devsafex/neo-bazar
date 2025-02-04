f1dd812201b0b340255e13636fb688b0054c8494
Mehefuj Ali <mehefujalim@gmail.com>
2025-02-05 02:27:53 +0530

    nav links added

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,26 +1,37 @@
 import { IoMdCart, IoMdSearch } from "react-icons/io";
 import Logo from "../../Shaired/Logo";
 import { IconButton } from "@material-tailwind/react";
+import Navlinks from "./Navlinks";
 
 const Nav = () => {
   return (
-    <div className=" bg-[#00C982]">
-      <div className=" container mx-auto flex items-center justify-between py-2">
-        <div>
-          <Logo whiteLogo />
-        </div>
-        <div>
-          <div className=" relative text-white">
-            <IoMdSearch  className=" text-2xl absolute top-1 md:top-2 right-2" />
-          <input type="text" placeholder="Search..." className=" focus:outline-0 border py-1 pr-8 w-full md:w-96 md:py-2  px-3 md:px-5 rounded" />
+    <div>
+      <div className=" bg-[#00C982] ">
+        <div className=" container w-11/12 gap-2  lg:w-full mx-auto flex items-center justify-between py-2">
+          <div>
+            <Logo whiteLogo />
+          </div>
+          <div>
+            <div className=" relative text-white">
+              <IoMdSearch className=" text-2xl absolute top-1 md:top-2 right-2" />
+              <input
+                type="text"
+                placeholder="Search..."
+                className=" focus:outline-0 border py-1 md:pr-8 pr-8 w-full md:w-96 md:py-2  px-2 md:px-5 rounded"
+              />
+            </div>
+          </div>
+          <div>
+            <IconButton
+              variant="text"
+              className="  rounded-full md:border-2  border-white  select-none h-10 w-10  cursor-pointer"
+            >
+              <IoMdCart className="  text-2xl text-white" />
+            </IconButton>
           </div>
         </div>
-        <div>
-          <IconButton variant="text" className="  rounded-full md:border-2  border-white  select-none h-10 w-10  cursor-pointer">
-            <IoMdCart className="  text-2xl text-white" />
-          </IconButton>
-        </div>
       </div>
+      <Navlinks></Navlinks>
     </div>
   );
 };
