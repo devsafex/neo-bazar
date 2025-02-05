c128059ba56efe667da5a7de612f4869f3a879da
Sanjida <sanjida.ice.778@gmail.com>
2025-02-06 01:50:22 +0600

    children added to button

--- src/Shaired/Button.jsx
+++ src/Shaired/Button.jsx
@@ -3,7 +3,7 @@ import { Button as Mtbtn } from "@material-tailwind/react";
 import { PiHandbagBold } from "react-icons/pi";
 // Don't chnange 
 export const Button = ({
-  childrens,
+  children,
   loading = false,
   size
 }) => {
@@ -12,12 +12,11 @@ export const Button = ({
       {/* Button form material tailwind  */}
       <Mtbtn
       size={size}
-        
         loading={loading}
         className={`bg-[#00C982] rounded flex items-center gap-3 cursor-pointer `}
       >
-        <PiHandbagBold className="text-2xl" />
-        {childrens}
+        
+        {children}
       </Mtbtn>
     </div>
   );
@@ -25,7 +24,7 @@ export const Button = ({
 
 // Prop validation 
 Button.propTypes = {
-  childrens: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
   loading: PropTypes.bool,
   size: PropTypes.string
 };
