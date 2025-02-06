f297b2250d56bd36da722956684bdaf73d0bdb33
Sanjida <sanjida.ice.778@gmail.com>
2025-02-06 06:04:40 +0600

    countdown added to sponsor section

--- src/Shaired/CountDown.jsx
+++ src/Shaired/CountDown.jsx
@@ -5,26 +5,32 @@ import Countdown from "react-countdown";
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
   if (completed) {
     // Render this when the countdown is complete
-    return <span className="text-red-500 font-bold">Expired!</span>;
+    return <span className="text-red-500/80 text-lg font-bold">Offer Expired!</span>;
   } else {
     // Render the countdown
+    //   <div className="flex justify-end text-gray-700 text-lg font-medium space-x-2 mt-4">
+    //           <span>281<span className="text-sm block">Days</span></span>
+    //           <span>23<span className="text-sm block">Hours</span></span>
+    //           <span>54<span className="text-sm block">Min</span></span>
+    //           <span>45<span className="text-sm block">Sec</span></span>
+    //         </div>
     return (
-      <div className="flex text-primary-color">
-        <span className="font-bold bg-primary-color bg-opacity-10 p-1 py-0">
-          {days}
+      <div className="flex text-[#00C982] my-8 text-2xl gap-2 text-center">
+        <span className="font-bold bg-primary-color bg-opacity-10 p-2 border">
+          {days}<span className="text-sm block text-black/60">Days</span>
         </span>
-        :
-        <span className="font-bold bg-primary-color bg-opacity-10 p-1 py-0">
-          {hours}
+        
+        <span className="font-bold bg-primary-color bg-opacity-10 p-2 border">
+          {hours}<span className="text-sm block text-black/60">Hours</span>
         </span>
-        :
-        <span className="font-bold bg-primary-color bg-opacity-10 p-1 py-0">
-          {minutes}
+        
+        <span className="font-bold bg-primary-color bg-opacity-10 p-2 border">
+          {minutes}<span className="text-sm block text-black/60">Min</span>
         </span>
-        :
-        <span className="font-bold bg-primary-color bg-opacity-10 p-1 py-0">
-          {seconds}
-        </span>
+        
+        <span className="font-bold bg-primary-color bg-opacity-10 p-2 border">
+          {seconds}<span className="text-sm block text-black/60">Sec</span>
+        </span>.
       </div>
     );
   }
--- src/components/SponsorsBanner/SponsorsBanner.jsx
+++ src/components/SponsorsBanner/SponsorsBanner.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Button from "../../Shaired/Button";
+import CountDown from "../../Shaired/CountDown";
 
 const SponsorsBanner = () => {
   const [sponsorData, setSponsorData] = useState({});
@@ -26,14 +27,9 @@ const SponsorsBanner = () => {
         <p className="text-gray-500 text-sm uppercase tracking-widest mb-4 relative e after:content-[''] after:w-12 after:h-[4px] after:bg-orange-500/60 after:rounded-2xl after:mb-1 after:inline-block after:ml-2 after:align-middle">Happy Sunday</p>
         <h2 className="text-xl md:text-3xl lg:text-7xl font-bold text-orange-500/60">40% <br></br> <span className="text-orange-500/60">OFF</span></h2>
         <p className="text-xl font-semibold text-gray-800 mt-1">All Modern Furnitures</p>
-        
         {/* Countdown Timer Placeholder */}
-        {/* <div className="flex justify-end text-gray-700 text-lg font-medium space-x-2 mt-4">
-          <span>281<span className="text-sm block">Days</span></span>
-          <span>23<span className="text-sm block">Hours</span></span>
-          <span>54<span className="text-sm block">Min</span></span>
-          <span>45<span className="text-sm block">Sec</span></span>
-        </div> */}
+        <CountDown time="2025-02-06T06:50:00"></CountDown>
+      
         
         {/* Shop Now Button */}
         <div className="mt-6">
