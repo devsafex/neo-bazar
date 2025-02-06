1b0179bc5e1a83081a35928ada3d71ee3a1b5fb0
Sanjida <sanjida.ice.778@gmail.com>
2025-02-06 06:43:52 +0600

    make the sponsor section responsive

--- src/Shaired/CountDown.jsx
+++ src/Shaired/CountDown.jsx
@@ -5,15 +5,8 @@ import Countdown from "react-countdown";
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
   if (completed) {
     // Render this when the countdown is complete
-    return <span className="text-red-500/80 text-lg font-bold">Offer Expired!</span>;
+    return <span className="text-red-500/80 font-bold">Offer Expired!</span>;
   } else {
-    // Render the countdown
-    //   <div className="flex justify-end text-gray-700 text-lg font-medium space-x-2 mt-4">
-    //           <span>281<span className="text-sm block">Days</span></span>
-    //           <span>23<span className="text-sm block">Hours</span></span>
-    //           <span>54<span className="text-sm block">Min</span></span>
-    //           <span>45<span className="text-sm block">Sec</span></span>
-    //         </div>
     return (
       <div className="flex text-[#00C982] my-8 text-2xl gap-2 text-center">
         <span className="font-bold bg-primary-color bg-opacity-10 p-2 border rounded-xs">
--- src/components/SponsorsBanner/SponsorsBanner.jsx
+++ src/components/SponsorsBanner/SponsorsBanner.jsx
@@ -17,19 +17,19 @@ const SponsorsBanner = () => {
 
   return (
     <div className="bg-[#00C982]/5">
-        <div className="container mx-auto my-10 md:my-16 lg:my-20 flex items-center justify-between p-10 relative overflow-hidden">
+        <div className="container mx-auto my-10 md:my-16 lg:my-20 grid grid-cols-1 md:grid-cols-5 items-center justify-between p-10 relative overflow-hidden">
       {/* Left Side - Image Section */}
-      <div className="relative w-3/5 flex items-center">
+      <div className="relative flex items-center col-span-1 md:col-span-3">
         <img src={sponsorData.img} alt="Sponsor" className="w-full h-auto" />
       </div>
       
       {/* Right Side - Text and Button */}
-      <div className="w-2/5 pl-10">
+      <div className="pl-0 md:pl-10 col-span-1 md:col-span-2">
         <p className="text-gray-500 text-sm uppercase tracking-widest mb-4 relative e after:content-[''] after:w-12 after:h-[4px] after:bg-orange-500/60 after:rounded-2xl after:mb-1 after:inline-block after:ml-2 after:align-middle">Happy Sunday</p>
-        <h2 className="text-xl md:text-3xl lg:text-7xl font-bold text-orange-500/60">40% <br></br> <span className="text-orange-500/60">OFF</span></h2>
+        <h2 className="text-2xl md:text-4xl lg:text-6xl xl:text-7xl font-bold text-orange-500/60">40% <br className="hidden md:block"/> <span className="text-orange-500/60">OFF</span></h2>
         <p className="text-xl font-semibold text-gray-800 mt-1">All Modern Furnitures</p>
         {/* Countdown Timer Placeholder */}
-        <CountDown time="2025-02-06T06:50:00"></CountDown>
+        <CountDown time="2025-02-08T06:20:00"></CountDown>
       
         
         {/* Shop Now Button */}
