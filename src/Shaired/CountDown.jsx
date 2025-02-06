33e636d23ad3b35189d956939006154f50c34df8
Sanjida <sanjida.ice.778@gmail.com>
2025-02-06 06:08:24 +0600

    customize countdown design

--- src/Shaired/CountDown.jsx
+++ src/Shaired/CountDown.jsx
@@ -16,20 +16,20 @@ const renderer = ({ days, hours, minutes, seconds, completed }) => {
     //         </div>
     return (
       <div className="flex text-[#00C982] my-8 text-2xl gap-2 text-center">
-        <span className="font-bold bg-primary-color bg-opacity-10 p-2 border">
+        <span className="font-bold bg-primary-color bg-opacity-10 p-2 border rounded-xs">
           {days}<span className="text-sm block text-black/60">Days</span>
         </span>
         
-        <span className="font-bold bg-primary-color bg-opacity-10 p-2 border">
+        <span className="font-bold bg-primary-color bg-opacity-10 p-2 border rounded-xs">
           {hours}<span className="text-sm block text-black/60">Hours</span>
         </span>
         
-        <span className="font-bold bg-primary-color bg-opacity-10 p-2 border">
-          {minutes}<span className="text-sm block text-black/60">Min</span>
+        <span className="font-bold bg-primary-color bg-opacity-10 p-2 border rounded-xs">
+          {minutes}<span className="text-sm block text-black/60">Mins</span>
         </span>
         
-        <span className="font-bold bg-primary-color bg-opacity-10 p-2 border">
-          {seconds}<span className="text-sm block text-black/60">Sec</span>
+        <span className="font-bold bg-primary-color bg-opacity-10 p-2 border rounded-xs">
+          {seconds}<span className="text-sm block text-black/60">Secs</span>
         </span>.
       </div>
     );
