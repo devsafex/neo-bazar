cd3907a1d462dac987439d300d0a567630743787
Mehefuj Ali <mehefujalim@gmail.com>
2025-02-07 23:45:19 +0530

    products route created

---
+++ src/Pages/Products/Products.jsx
@@ -0,0 +1,11 @@
+import ProductCard from "../../components/ProductCard/ProductCard";
+
+const Products = () => {
+      return (
+            <div>
+                 <ProductCard/> 
+            </div>
+      );
+};
+
+export default Products;
