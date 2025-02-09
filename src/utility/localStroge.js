4441a0a79497d275f45ac9df9f25351f86538227
Mehefuj Ali <mehefujalim@gmail.com>
2025-02-10 00:13:09 +0530

    local stroge set item created

--- src/utility/localStroge.js
+++ src/utility/localStroge.js
@@ -2,3 +2,16 @@ const getItemFromLocal = (key) => {
   const localItem = JSON.parse(localStorage.getItem(key));
   return localItem;
 };
+const setItemToLocal = (key, item) => {
+      let localItems = getItemFromLocal(key);
+      const existingItem = localItems.find((localItem) => localItem === item);
+      if (existingItem) {
+        return;
+      } else {
+        localItems.push(item);
+        localStorage.setItem(key, localItems);
+      }
+    };
+    
+    export { getItemFromLocal, setItemToLocal };
+    
