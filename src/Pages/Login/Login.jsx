e82dcb19e2ce2be9f4fd697412214468695c0e08
Sanjida <sanjida.ice.778@gmail.com>
2025-02-10 03:22:09 +0600

    login and register done with firebase

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,26 +1,76 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import { Link, useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 import { CgSpinnerAlt } from "react-icons/cg";
+import { useState } from "react";
+import useAuth from "../../hooks/useAuth";
 
 const Login = () => {
-  const { user, loginWithRedirect, isAuthenticated, logout, isLoading  } = useAuth0();
-  console.log(user);
+  const { signIn, signInWithGoogle, loading, setLoading, setUser } = useAuth();
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError("");
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    
+    try {
+      const res = await signIn(email, password);
+      setUser(res?.user);
+      navigate("/", { replace: true });
+    } catch (err) {
+      setLoading(false);
+      setError(err?.message);
+    }
+  };
+
+  const handleGoogleSignIn = async () => {
+    try {
+      await signInWithGoogle();
+      navigate("/", { replace: true });
+    } catch (err) {
+      setLoading(false);
+      setError(err?.message);
+    }
+  };
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className=" p-8 rounded-lg shadow-lg text-center max-w-sm w-full">
-        <h2 className="text-3xl font-bold text-[#00C982]">Welcome Back</h2>
-        <p className="text-gray-400 mt-2">Login to continue</p>
-
-        {isAuthenticated ? ( 
-          <button onClick={() => logout()}>{isLoading ? <CgSpinnerAlt className="animate-spin m-auto" />:'LOGOUT'}</button>
-        ) : (
-          <button
-            onClick={() => loginWithRedirect()}
-            className="mt-6 w-full bg-[#00C982] text-white py-3 rounded-md font-semibold hover:bg-[#00a06e] transition"
-          >
-            Login with Auth0
-          </button>
-        )}
+    <div className="flex flex-col lg:flex-row gap-20 my-10 md:my-16 lg:my-20 justify-center items-center min-h-screen bg-white">
+      <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-[#00C982]/5 text-gray-900">
+        <div className="mb-8 text-center">
+          <h1 className="my-3 text-4xl font-bold">Log In</h1>
+          <p className="text-sm text-gray-400">Sign in to access your account</p>
+        </div>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="space-y-4">
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm">Email address</label>
+              <input type="email" name="email" id="email" required placeholder="Enter Your Email Here" className="w-full px-3 py-2 input input-bordered focus:outline-[#00C982] text-gray-900" />
+            </div>
+            <div>
+              <label htmlFor="password" className="text-sm mb-2">Password</label>
+              <input type="password" name="password" autoComplete="current-password" id="password" required placeholder="*******" className="w-full px-3 py-2 input input-bordered focus:outline-[#00C982] text-gray-900" />
+            </div>
+            {error && <p className="mb-2 text-red-500 text-sm">{error}</p>}
+          </div>
+          <div>
+            <button type="submit" className="bg-[#00C982] hover:bg-[#009F66] w-full rounded-md py-3 text-white font-bold">
+              {loading ? <CgSpinnerAlt className="animate-spin m-auto" /> : "Login"}
+            </button>
+          </div>
+        </form>
+        <div className="space-y-1 my-2">
+          <button className="text-xs hover:underline hover:text-[#00C982] text-gray-400">Forgot password?</button>
+        </div>
+        <div onClick={handleGoogleSignIn} className="flex justify-center border-[#00C982] items-center space-x-2 border my-3 p-2 rounded-lg cursor-pointer">
+          <FcGoogle size={32} />
+          <p>Continue with Google</p>
+        </div>
+        <p className="px-6 text-sm text-center text-gray-400">
+          Don't have an account yet? <Link to="/signup" className="hover:underline hover:text-[#00C982] font-bold text-gray-600">Sign up</Link>.
+        </p>
       </div>
     </div>
   );
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,25 +1,165 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import { Link, useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+// import useAuth from "../../hooks/useAuth";
+// import { toast } from "react-hot-toast";
+// import { imageUpload, saveUser } from "../../utilities/utils";
+import { useState } from "react";
+import { CgSpinnerAlt } from "react-icons/cg";
+import useAuth from "../../hooks/useAuth";
 
 const SignUp = () => {
-  const { loginWithRedirect } = useAuth0();
-
-  const handleSignup = () => {
-    loginWithRedirect({
-      screen_hint: "signup", // This will force the signup screen
-      redirect_uri: window.location.origin, // Optional: to specify where to redirect after successful login/signup
-    });
+  
+  const [error, setError] = useState("");
+  const { createUser, signInWithGoogle, loading, setLoading } =
+    useAuth();
+const navigate = useNavigate()
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError('')  
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    
+    if (password.length < 6) {
+      setError("Length must be at least 6 character ");
+    }
+    if (!/[a-z]/.test(password)) {
+      setError("Password must contain at least one lowercase letter");
+      return;
+    }
+    if (!/[A-Z]/.test(password)) {
+      setError("Password must contain at least one uppercase letter");
+      return;
+    }
+    if (!/[0-9]/.test(password)) {
+      setError("Password must contain at least one digit");
+      return;
+    }
+    
+    try {
+      await createUser(email, password);
+      navigate('/')
+    } catch (error) {
+      setLoading(false)
+      console.log(error?.message)
+    }
   };
 
+  // Handle Google Signin
+  const handleGoogleSignIn = async () => {
+    try {
+      await signInWithGoogle();
+    } catch (error) {
+      setLoading(false)
+      console.log(error?.message)
+    }
+  };
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-2xl shadow-lg max-w-sm w-full">
-        <h2 className="text-2xl font-bold text-gray-800 text-center mb-6">Create Account</h2>
-        
-        <button 
-          onClick={handleSignup} 
-          className="w-full flex items-center justify-center space-x-2 bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-lg shadow">
-          <span>Sign Up with Auth0</span>
-        </button>
+    <div className="flex flex-col lg:flex-row gap-20 my-10 md:my-16 lg:my-20 justify-center items-center min-h-screen bg-white">
+     
+      <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-[#00C982]/5 text-gray-900">
+        <div className="mb-8 text-center">
+          <h1 className="my-3 text-4xl font-bold">Sign Up</h1>
+          <p className="text-sm text-gray-400">Welcome to Neo Bazar</p>
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          noValidate=""
+          action=""
+          className="space-y-6 ng-untouched ng-pristine ng-valid"
+        >
+          <div className="space-y-4">
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm">
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Enter Your Name Here"
+                className="w-full px-3 py-2 input input-bordered focus:outline-[#00C982] text-gray-900"
+                data-temp-mail-org="0"
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm">
+                Email address
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                placeholder="Enter Your Email Here"
+                className="w-full px-3 py-2 input input-bordered focus:outline-[#00C982] text-gray-900"
+                data-temp-mail-org="0"
+              />
+            </div>
+            <div>
+              <label htmlFor="image" className="block mb-2 text-sm">
+                Select Image:
+              </label>
+              <input
+                required
+                type="file"
+                id="image"
+                name="image"
+                accept="image/*"
+              />
+            </div>
+           
+            <div>
+              <div className="flex justify-between">
+                <label htmlFor="password" className="text-sm mb-2">
+                  Password
+                </label>
+              </div>
+              <input
+                type="password"
+                name="password"
+                autoComplete="new-password"
+                id="password"
+                required
+                placeholder="*******"
+                className="w-full px-3 py-2 border input input-bordered rounded-md focus:outline-[#00C982] text-gray-900"
+              />
+            </div>
+            {error && <p className="mb-2 text-red-500 text-sm">{error}</p>}
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="bg-[#00C982] hover:bg-[#009F66] w-full rounded-md py-3 text-white font-bold"
+            >
+              {loading ? (
+                <CgSpinnerAlt className="animate-spin m-auto" />
+              ) : (
+                "Register"
+              )}
+            </button>
+          </div>
+        </form>
+
+        <div
+          onClick={handleGoogleSignIn}
+          className="flex justify-center border-[#00C982] items-center space-x-2  border my-3 p-2 rounded-lg cursor-pointer"
+        >
+          <FcGoogle size={32} />
+          <p>Continue with Google</p>
+        </div>
+        <p className="px-6 text-sm text-center text-gray-400">
+          Already have an account?
+          <Link
+            to="/login"
+            className="hover:underline hover:text-[#00C982] font-bold text-gray-600"
+          >
+            Login
+          </Link>
+          .
+        </p>
       </div>
     </div>
   );
