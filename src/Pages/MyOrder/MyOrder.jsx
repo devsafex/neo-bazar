3da5ceae1a79ccaaa5a7f0cd7048b211d85a5659
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-02-13 20:47:19 +0530

    tablet responsiveness is created

--- src/Pages/MyOrder/MyOrder.jsx
+++ src/Pages/MyOrder/MyOrder.jsx
@@ -11,7 +11,7 @@ const MyOrder = () => {
       {/* <h2 className="text-2xl font-light">Orders History and Status</h2> */}
       <div className="grid grid-cols-12 gap-3">
         {/* side container */}
-        <div className="col-span-2 mt-4 border-2 p-3 min-h-52">
+        <div className="md:col-span-3 lg:col-span-2 mt-4 border-2 p-3 min-h-52">
           <h3 className=" text-2xl font-semibold mb-2">Filters</h3>
           <h4 className="text-gray-500 text-lg mb-2">Order Status</h4>
           <div className="flex flex-col ">
@@ -30,7 +30,7 @@ const MyOrder = () => {
           </div>
         </div>
         {/* min box  */}
-        <div className=" col-span-10 mt-4 p-3 border-2 min-h-screen">
+        <div className="md:col-span-9 lg:col-span-10 mt-4 p-3 border-2 min-h-screen">
           {/* search container  */}
           <div className="flex  items-center">
             <input
@@ -52,6 +52,7 @@ const MyOrder = () => {
               )}
             </Button>
           </div>
+          {/* order history  */}
         </div>
       </div>
     </div>
