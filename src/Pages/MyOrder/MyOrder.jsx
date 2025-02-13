8dbda3df412b456478e2958f8d341285a01841e6
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-02-13 20:37:05 +0530

    Search button is implemented in my order section

--- src/Pages/MyOrder/MyOrder.jsx
+++ src/Pages/MyOrder/MyOrder.jsx
@@ -1,11 +1,17 @@
-import { Checkbox } from "@material-tailwind/react";
+import { Button, Checkbox, Spinner } from "@material-tailwind/react";
+import { useState } from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 const MyOrder = () => {
+  const [loading, setLoading] = useState(false);
+  const handleClick = () => {
+    setLoading(true);
+  };
   return (
     <div className="container lg:w-11/12  mx-auto w-full mt-8">
       {/* <h2 className="text-2xl font-light">Orders History and Status</h2> */}
       <div className="grid grid-cols-12 gap-3">
         {/* side container */}
-        <div className="col-span-2 mt-4 bg- p-3 min-h-52">
+        <div className="col-span-2 mt-4 border-2 p-3 min-h-52">
           <h3 className=" text-2xl font-semibold mb-2">Filters</h3>
           <h4 className="text-gray-500 text-lg mb-2">Order Status</h4>
           <div className="flex flex-col ">
@@ -24,7 +30,29 @@ const MyOrder = () => {
           </div>
         </div>
         {/* min box  */}
-        <div className=" col-span-10"> </div>
+        <div className=" col-span-10 mt-4 p-3 border-2 min-h-screen">
+          {/* search container  */}
+          <div className="flex  items-center">
+            <input
+              type="text"
+              className="border-2 border-[#00C982] w-full rounded-l-lg p-2 "
+            />
+            <Button
+              onClick={handleClick}
+              className="flex bg-[#00C982] rounded-r-lg rounded-l-none  items-center gap-1"
+            >
+              {loading ? (
+                <div className="w-16 h-4 flex items-center gap-1">
+                  <Spinner className="border-[#00C982]" /> Loading
+                </div>
+              ) : (
+                <>
+                  <FaMagnifyingGlass /> Search
+                </>
+              )}
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
