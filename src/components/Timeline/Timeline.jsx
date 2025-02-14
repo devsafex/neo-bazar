4d93a3d071fbf7279ec194ca6f1a4b21548a45da
Sanjida <sanjida.ice.778@gmail.com>
2025-02-14 23:35:50 +0600

    make horizontal line

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -16,7 +16,7 @@ import {
     return (
       <div className="w-full max-w-7xl mx-auto relative py-10 px-4">
         {/* Vertical Line */}
-        <div className="absolute w-9/12 left-24 top-17 h-[2px] bg-[#00C982] hidden lg:block"></div>
+        <div className="absolute w-10/12 left-24 top-17 h-[2px] bg-[#00C982] hidden lg:block"></div>
   
         <Timeline className="grid lg:grid-cols-5 grid-cols-1 gap-10">
           {/* Timeline Item 1 */}
@@ -24,16 +24,16 @@ import {
             <TimelineIcon className="p-3 bg-[#00C982] text-white rounded-full shadow-md mx-auto">
               <SiGnuprivacyguard className="h-8 w-8" />
             </TimelineIcon>
-            <TimelineHeader className="w-full">
-              <Typography variant="h6" color="blue-gray" className="mt-4 font-bold w-full">
+            <TimelineHeader className="w-full mt-3">
+              <Typography variant="h6" color="blue-gray" className="mt-4 font-bold w-full text-center">
                 Signup for Free
               </Typography>
             </TimelineHeader>
-            <div className="mt-2 w-full gap-0">
-              <p className="font-normal text-start text-gray-600">
+            <p className="mt-2 w-full gap-0">
+              <Typography className="font-normal text-gray-600 text-justify">
                 Create your account through our website or mobile app with just your phone number.
-              </p>
-            </div>
+              </Typography>
+            </p>
           </TimelineItem>
   
           {/* Timeline Item 2 */}
@@ -41,13 +41,13 @@ import {
             <TimelineIcon className="p-3 mx-auto bg-[#00C982] text-white rounded-full shadow-md">
               <IoPersonAdd className="h-8 w-8" />
             </TimelineIcon>
-            <TimelineHeader className="w-full">
-              <Typography variant="h6" color="blue-gray" className="mt-4 font-bold">
+            <TimelineHeader className="w-full mt-3">
+              <Typography variant="h6" color="blue-gray" className="mt-4 font-bold w-full text-center">
                 Add Profile Information
               </Typography>
             </TimelineHeader>
             <p className="mt-2">
-              <Typography className="font-normal text-gray-600">
+              <Typography className="font-normal text-gray-600 text-justify">
                 Complete your profile by providing your email and store name so that we can identify you.
               </Typography>
             </p>
@@ -58,13 +58,13 @@ import {
             <TimelineIcon className="p-3 mx-auto bg-[#00C982] text-white rounded-full shadow-md">
               <FaAddressBook className="h-8 w-8" />
             </TimelineIcon>
-            <TimelineHeader className="w-full">
-              <Typography variant="h6" color="blue-gray" className="mt-4 font-bold">
+            <TimelineHeader className="w-full mt-3">
+              <Typography variant="h6" color="blue-gray" className="mt-4 font-bold w-full text-center">
                 Add Address Information
               </Typography>
             </TimelineHeader>
             <p className="mt-2">
-              <Typography className="font-normal text-gray-600">
+              <Typography className="font-normal text-gray-600 text-justify">
                 Provide all address details of your business.
               </Typography>
             </p>
@@ -75,13 +75,13 @@ import {
             <TimelineIcon className="p-3 mx-auto bg-[#00C982] text-white rounded-full shadow-md">
               <FaAddressCard className="h-8 w-8" />
             </TimelineIcon>
-            <TimelineHeader className="w-full">
-              <Typography variant="h6" color="blue-gray" className="mt-4 font-bold">
+            <TimelineHeader className="w-full mt-3">
+              <Typography variant="h6" color="blue-gray" className="mt-4 font-bold w-full text-center">
                 Add ID & Bank Information
               </Typography>
             </TimelineHeader>
             <p className="mt-2">
-              <Typography className="font-normal text-gray-600">
+              <Typography className="font-normal text-gray-600 text-justify">
                 Add your ID & business details, including necessary bank information for payments.
               </Typography>
             </p>
@@ -92,13 +92,13 @@ import {
             <TimelineIcon className="p-3 mx-auto bg-[#00C982] text-white rounded-full shadow-md">
               <RiFileList3Fill className="h-8 w-8" />
             </TimelineIcon>
-            <TimelineHeader className="w-full">
-              <Typography variant="h6" color="blue-gray" className="mt-4 font-bold">
+            <TimelineHeader className="w-full mt-3">
+              <Typography variant="h6" color="blue-gray" className="mt-4 font-bold w-full text-center">
                 List Products
               </Typography>
             </TimelineHeader>
             <p className="mt-2">
-              <Typography className="font-normal text-gray-600">
+              <Typography className="font-normal text-gray-600 text-justify">
                 Add products to your store via the seller center. Start selling once your products go live after quality control.
               </Typography>
             </p>
